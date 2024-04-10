c916d06c46074d718699137df8e2b4c8c2cb77e9
hyx <huangyx1229@163.com>
2024-04-10 22:16:05 +0800

    触发ci

--- build/build-images-define.ps1
+++ build/build-images-define.ps1
@@ -2,7 +2,7 @@ $onlyPrint = $false
 
 # 编译镜像 linux/amd64
 $buildImageList = @(
-    # ###
+    # ##
     # "aspnet:6-centos-7.9.2009-gdi-fontconfig",
     # "aspnet:6-focal-gdi-fontconfig",
     # "aspnet:5-focal-gdi-fontconfig",
