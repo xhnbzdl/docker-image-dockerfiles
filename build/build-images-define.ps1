d8584cd1ca81c77d2f8109dcaad280210d230da3
hyx <huangyx1229@163.com>
2024-04-13 21:27:31 +0800

    pwsh:lts-windowsservercore-ltsc2022-docker-24.0.2

--- build/build-images-define.ps1
+++ build/build-images-define.ps1
@@ -24,7 +24,8 @@ $buildImageList = @(
     # "node:14.21.3",
     # "node:16.13.1", #阿里云的不可用，docker hub可用
     # "node:20.10.0",
-    "powershell:lts-windowsservercore-ltsc2022",
+    # "powershell:lts-windowsservercore-ltsc2022",
+    # "powershell:lts-windowsservercore-ltsc2022-docker-24.0.2",
     # "powershell:lts-debian-10-focal",
     # "powershell:lts-debian-10-focal-node-14",
     # "powershell:lts-debian-10-focal-node-16",
